27ebf0aa119d1a8bf51c50a4ada921271b76e1b8
lokimuj <lokimuj9@gmail.com>
2018-03-02 15:11:39 -0500

    Starting Board implementation

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -1,4 +1,34 @@
 package com.webcheckers.model;
 
-public class Board {
+import java.util.*;
+
+public class Board implements Iterable<Row> {
+
+    private static final int NUM_ROWS = 8;
+
+    private Player curPlayer;
+    private Player redPlayer;
+    private Set<Piece> redPieces;
+    private Player whitePlayer;
+    private Set<Piece> whitePieces;
+    private List<Row> rows;
+
+    public Board(Player redPlayer, Player whitePlayer){
+        this.redPlayer = redPlayer;
+        this.redPieces = new HashSet<>();
+        this.whitePlayer = whitePlayer;
+        this.whitePieces = new HashSet<>();
+        this.curPlayer = redPlayer;
+        this.rows = new ArrayList<>(NUM_ROWS);
+        for(int i = 0;i<NUM_ROWS;i++){
+            rows.add(new Row());
+        }
+
+
+    }
+
+    @Override
+    public Iterator<Row> iterator() {
+        return rows.iterator();
+    }
 }
