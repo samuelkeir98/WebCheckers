c7c5033bde7d4380417be543c74ac8e4625f76c6
lokimuj <lokimuj9@gmail.com>
2018-03-03 14:59:47 -0500

    Updated move to have third category

--- src/main/java/com/webcheckers/model/moves/Move.java
+++ src/main/java/com/webcheckers/model/moves/Move.java
@@ -3,32 +3,17 @@ package com.webcheckers.model.moves;
 import com.webcheckers.model.Color;
 import com.webcheckers.model.Piece;
 
+import java.util.Objects;
+
 /**
  * Value object representing one move
  * @author Adrian Postolache
  */
 public abstract class Move {
 
-    /**
-     * Represent whether a move is a one-tile step or two-tile jump.
-     * Includes those values to keep the actual code generic.
-     */
-    public enum Type{
-        STEP(1), //Regular, one-tile movement
-        JUMP(2); //Attacking, two-tile movement
-
-        /**
-         * How many spaces this type of move will take.
-         */
-        private int spaces;
-
-        Type(int spaces){
-            this.spaces=spaces;
-        }
-
-        public int getSpaces() {
-            return spaces;
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPos.hashCode(),endPos.hashCode());
     }
 
     Position startPos,endPos;
@@ -49,5 +34,36 @@ public abstract class Move {
 
     public Type getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Move){
+            Move move = (Move) obj;
+            return startPos.equals(move.startPos) && endPos.equals(move.endPos);
+        }
+        return false;
+    }
+
+    /**
+     * Represent whether a move is a one-tile step or two-tile jump.
+     * Includes those values to keep the actual code generic.
+     */
+    public enum Type{
+        STEP(1), //Regular, one-tile movement
+        JUMP(2), //Attacking, two-tile movement
+        OTHER(0); //Used for player-input moves that may or may not be valid
+        /**
+         * How many spaces this type of move will take.
+         */
+        private int spaces;
+
+        Type(int spaces){
+            this.spaces=spaces;
+        }
+
+        public int getSpaces() {
+            return spaces;
+        }
     }
 }
