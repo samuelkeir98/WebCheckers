63eca15b642cc8357902a29888de86d9cf300c33
lokimuj <lokimuj9@gmail.com>
2018-03-04 18:42:15 -0500

    Doc's KingDirection and most of Board

--- src/main/java/com/webcheckers/ui/PostGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostGameRoute.java
@@ -3,7 +3,6 @@ package com.webcheckers.ui;
 import com.webcheckers.appl.BoardView;
 import com.webcheckers.appl.GameLobby;
 import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.model.Board;
 import com.webcheckers.model.Color;
 import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
