8bda7932068878e4c79f2a80d589e0742410d55f
lokimuj <lokimuj9@gmail.com>
2018-03-25 14:34:57 -0400

    Started some model-tier unit tests

---
+++ src/test/java/com/webcheckers/model/GameTester.java
@@ -0,0 +1,11 @@
+package com.webcheckers.model;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Tag("Model-Tier")
+public class GameTester {
+}
