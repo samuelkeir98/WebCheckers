473294188ee712f407bdcd223d0c0806ac5c266b
lokimuj <lokimuj9@gmail.com>
2018-04-01 15:55:03 -0400

    massive commit you like it

--- src/test/java/com/webcheckers/model/BoardTester.java
+++ src/test/java/com/webcheckers/model/BoardTester.java
@@ -2,6 +2,7 @@ package com.webcheckers.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.webcheckers.model.moves.Move;
 import com.webcheckers.model.moves.Position;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,25 @@ public class BoardTester {
 				}
 			}
 		}
+	}
+
+	@Test
+	public void stepMoveTest(){
+
+		Move move1 = new Move(new Position(7,7),new Position(6,6));
+		Move invalid = new Move(new Position(7,7),new Position(5,5));
+		Board board = Board.emptyBoard();
+		Piece piece = new Piece(new Position(7,7),Color.RED);
+		board.placePiece(piece, new Position(7,7));
+		assertEquals(piece,board.getPiece(new Position(7,7)));
+		board.addMoves();
+		assertFalse(board.isTurnOver());
+		assertTrue(board.isValidMove(move1));
+		assertFalse(board.isValidMove(invalid));
+		Move actual = board.getMove(move1);
+		board.makeMove(actual);
+		assertNull(board.getPiece(new Position(7,7)));
+		assertEquals(piece,board.getPiece(new Position(6,6)));
 	}
 
 }
