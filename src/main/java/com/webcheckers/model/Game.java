85e8c09c053df0637b688196b2bb9b7e7739b1af
Sam Keir <samuelkeir98@gmail.com>
2018-03-31 21:18:46 -0400

    Added initial check

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -73,13 +73,16 @@ public class Game {
      * Turns a normal piece into a king piece
      */
     public void makeKing(Piece piece){
-        if(piece.getColor() == Color.RED){
-            if(piece.getPosition().getRow() == redEnd){
+        if (piece.getType() == Piece.Type.KING){
+            return;
+        }
+        if (piece.getColor() == Color.RED){
+            if (piece.getPosition().getRow() == redEnd){
                 piece.becomeKing();
             }
         }
-        if(piece.getColor() == Color.WHITE){
-            if(piece.getPosition().getRow() == whiteEnd){
+        if (piece.getColor() == Color.WHITE){
+            if (piece.getPosition().getRow() == whiteEnd){
                 piece.becomeKing();
             }
         }
