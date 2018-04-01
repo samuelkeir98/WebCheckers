9d1bbedfe07187e1c3f77dd9f1a94121166e2e1c
Andy <axg5351@rit.edu>
2018-04-01 18:10:33 -0400

    More Unit tests

--- src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -1,11 +1,7 @@
 package com.webcheckers.ui;
 
-import com.webcheckers.appl.BoardView;
 import com.webcheckers.appl.GameLobby;
 import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.model.Board;
-import com.webcheckers.model.Color;
-import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +13,6 @@ import spark.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 
 /**
  * Unit test suite for the GetHome component.
@@ -27,25 +22,20 @@ import static org.mockito.ArgumentMatchers.eq;
 public class GetHomeRouteTest {
 
     private static final String PLAYER_NAME = "name";
-    private static final String OPPONENT = "opponent";
 
     /** The component-under-test (CuT) */
-    private GetGameRoute CuT;
+    private GetHomeRoute CuT;
 
     //friendly objects
     private Player player;
-    private Player opponent;
-    private Board board;
 
     //mock objects
     private Request request;
     private Session session;
     private Response response;
     private TemplateEngine engine;
-    private GameLobby gameLobby;
-    private Game game;
     private PlayerLobby playerLobby;
-    private Color color;
+    private GameLobby gameLobby;
 
     /**
      * Setup mock objects before each test
@@ -57,30 +47,25 @@ public class GetHomeRouteTest {
         session = mock(Session.class);
         response = mock(Response.class);
         engine = mock(TemplateEngine.class);
-        gameLobby = mock(GameLobby.class);
-        game = mock(Game.class);
         playerLobby = mock(PlayerLobby.class);
-        //opponent = mock(Player.class);
+        gameLobby = mock(GameLobby.class);
 
         //friendlies
         player = new Player(PLAYER_NAME);
-        opponent = new Player(OPPONENT);
-        board = new Board();
-        color = Color.RED;
 
         //return certain params when functions called
         when(request.session()).thenReturn(session);
 
         //setup test
-        CuT = new GetGameRoute(engine, gameLobby);
+        CuT = new GetHomeRoute(engine, playerLobby, gameLobby);
     }
 
     /**
-     * Test player is in game
+     * Player signed out
      */
     @Test
-    public void game_entered() {
-        when(session.attribute(PostSigninRoute.PLAYER_KEY)).thenReturn(player);
+    public void signed_Out() {
+        when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(null);
 
         //mock render for analysis
         final TemplateEngineTester testHelper = new TemplateEngineTester();
@@ -94,40 +79,69 @@ public class GetHomeRouteTest {
         testHelper.assertViewModelExists();
         testHelper.assertViewModelIsaMap();
         //model contains all necessary View-Model data
-        testHelper.assertViewModelAttribute(PostGameRoute.CURRENT_PLAYER_ATTR, player);
-        testHelper.assertViewModelAttribute(PostGameRoute.VIEW_MODE_ATTR, ViewMode.PLAY);
-        testHelper.assertViewModelAttribute(PostGameRoute.RED_PLAYER_ATTR, opponent);
-        testHelper.assertViewModelAttribute(PostGameRoute.WHITE_PLAYER_ATTR, player);
-        testHelper.assertViewModelAttribute(PostGameRoute.ACTIVE_COLOR_ATTR, color);
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        testHelper.assertViewModelAttribute(GetHomeRoute.NUM_PLAYER_PARAM, playerLobby.numPlayers());
 
-        testHelper.assertViewModelAttribute(PostGameRoute.BOARD_ATTR, new BoardView(board, Color.WHITE));
         //test view name
-        testHelper.assertViewName(PostGameRoute.TEMPLATE_NAME);
+        testHelper.assertViewName(GetHomeRoute.TEMPLATE_NAME);
     }
 
     /**
-     * Test that the "challenge" action fails to enter game (opponent in game)
+     * Player signed in, num players is 0, player not in game
      */
     @Test
-    public void game_not_entered_1() {
-        //test scenario: opponent in game
-        when(gameLobby.inGame(eq(player))).thenReturn(false);
+    public void noOtherPlayers() {
+        when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player);
+        when(playerLobby.numPlayers()).thenReturn(1);
+        when(gameLobby.inGame(player)).thenReturn(false);
+
+        //mock render for analysis
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
 
         //invoke test
         CuT.handle(request, response);
 
-        //response redirected
+        //Analyze the results:
+        //model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //model contains all necessary View-Model data
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        testHelper.assertViewModelAttribute(GetHomeRoute.USER_PARAM, player);
+        testHelper.assertViewModelAttribute(GetHomeRoute.NUM_PLAYER_PARAM, playerLobby.numPlayers());
+
+        //test view name
+        testHelper.assertViewName(GetHomeRoute.TEMPLATE_NAME);
     }
 
     /**
-     * Test that the player has signed out
+     * Player signed in, num players is > 1, player not in game
      */
     @Test
-    public void game_not_entered_2() throws NullPointerException {
-        //test scenario: opponent logged out
-        when(gameLobby.inGame(eq(player))).thenThrow(NullPointerException.class);
+    public void otherUsersPresent() {
+        when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player);
+        when(playerLobby.numPlayers()).thenReturn(2);
+        when(gameLobby.inGame(player)).thenReturn(false);
 
-        //response redirected
+        //mock render for analysis
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //invoke test
+        CuT.handle(request, response);
+
+        //Analyze the results:
+        //model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //model contains all necessary View-Model data
+        testHelper.assertViewModelAttribute("title", "Welcome!");
+        testHelper.assertViewModelAttribute(GetHomeRoute.USER_PARAM, player);
+        testHelper.assertViewModelAttribute(GetHomeRoute.NUM_PLAYER_PARAM, playerLobby.numPlayers());
+
+        //test view name
+        testHelper.assertViewName(GetHomeRoute.TEMPLATE_NAME);
     }
 
 }
